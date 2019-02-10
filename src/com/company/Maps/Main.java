cb8913d73c0c6438af9d54adee34a75a7585e0e4
ngweihow <ngweihow.r@gmail.com>
2019-02-10 23:28:12 +1100

    Map practise

---
+++ src/com/company/Maps/Main.java
@@ -0,0 +1,24 @@
+package com.company.Maps;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Class to revise on different types of maps
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Map<String, String> linkedHashMap = new LinkedHashMap<>();
+        Map<String, String> hashMap = new HashMap<>();
+        Map<String, String> treeMap = new TreeMap<>();
+
+    }
+
+
+
+}
