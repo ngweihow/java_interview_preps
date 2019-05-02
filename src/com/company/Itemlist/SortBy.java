24752e77e6ed5102b8661133a6bb0b0458cfc3dc
ngweihow <ngweihow.r@gmail.com>
2019-05-02 15:50:13 +1000

    Interface added

---
+++ src/com/company/Itemlist/SortBy.java
@@ -0,0 +1,14 @@
+package com.company.Itemlist;
+
+import java.util.List;
+
+public interface SortBy {
+
+    /**
+     * Sort the list accordingly.
+     * @param list List of Item objects to be rearranged.
+     */
+    void sortListBy(List<Item> list);
+
+    void print(List<Item> list);
+}
---
+++ src/com/company/Itemlist/SortByName.java
@@ -0,0 +1,17 @@
+package com.company.Itemlist;
+
+import java.util.List;
+
+public class SortByName implements SortBy{
+    boolean sorted;
+
+    @Override
+    public void sortListBy(List<Item> list) {
+
+    }
+
+    @Override
+    public void print(List<Item> list) {
+
+    }
+}
